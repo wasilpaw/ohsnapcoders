77f56bb315846e13e094dd1aebebf927df479ed2
Pawel Wasilewski <35202418+wasilpaw@users.noreply.github.com>
2021-04-25 21:04:33 +0100

    Update MetalDetector.py

--- MetalDetector.py
+++ MetalDetector.py
@@ -1,9 +1,12 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
-import time
 
-DEPTH = 10 # how deep will the metal detactor check
+DEPTH = 5 # how deep will the metal detactor check
 WIDTH = 3 # how far to each of the side to check
+
+metals = {}
+metals[block.GOLD_BLOCK.id]="Gold"      # {block.GOLD_BLOCK.id: 'Gold'}    
+metals[block.DIAMOND_ORE.id]="Diamond"  # {block.GOLD_BLOCK.id: 'Gold', block.DIAMOND_ORE.id: 'Diamond'}
 
 mc = minecraft.Minecraft.create()
 
@@ -12,7 +15,7 @@ while True:
     for y in range(pos.y-DEPTH,pos.y):
         for x in range(pos.x-WIDTH,pos.x+WIDTH):
             b = mc.getBlock(x,y,pos.z)
-            if b==block.GOLD_BLOCK.id:
-                mc.postToChat("Golden Block detected at x=" + str(x)+" y="+str(y)+" z="+str(pos.z))
-            
-            
+            if b in metals.keys():
+                mc.postToChat(metals[b]+" detected at x=" + str(x)+" y="+str(y)+" z="+str(pos.z))
+                if x == pos.x:
+                    mc.postToChat("!!!")
